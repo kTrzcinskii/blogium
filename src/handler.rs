9e7d3efea9bfd610241601ebaffa49c55b0e99c8
kTrzcinskii <kt.trzcinski@gmail.com>
2024-10-24 00:36:22 +0200

    Setup frontend

--- src/handler.rs
+++ src/handler.rs
@@ -1,30 +1,10 @@
-use axum::{
-    extract::State,
-    http,
-    response::{IntoResponse, Redirect},
-    Form, Json,
-};
+use axum::{extract::State, http, response::IntoResponse, Json};
 
 use crate::{errors::ServerError, schema::CreatePostSchema, state::ArcAppState};
 
-pub async fn home_page_handler(
-    State(app_state): State<ArcAppState>,
-) -> Result<impl IntoResponse, (http::StatusCode, Json<serde_json::Value>)> {
-    let row: (i64,) = sqlx::query_as("SELECT COUNT(*) FROM posts")
-        .fetch_one(&app_state.db)
-        .await
-        .unwrap();
-
-    Ok(format!("Posts count: {}", row.0))
-}
-
-pub async fn redirect_to_home_handler() -> impl IntoResponse {
-    Redirect::to("/home")
-}
-
 pub async fn create_post_handler(
     State(app_state): State<ArcAppState>,
-    Form(body): Form<CreatePostSchema>,
+    Json(body): Json<CreatePostSchema>,
 ) -> Result<impl IntoResponse, ServerError> {
     // TODO: handle images
     let result = sqlx::query(r#"INSERT INTO posts (username, content) VALUES (?, ?)"#)
--- src/route.rs
+++ src/route.rs
@@ -1,16 +1,11 @@
 use std::sync::Arc;
 
-use axum::{
-    routing::{get, post},
-    Router,
-};
+use axum::{routing::post, Router};
 
 use crate::{handler, state::AppState};
 
 pub fn create_router(app_state: Arc<AppState>) -> Router {
     Router::new()
-        .route("/", get(handler::redirect_to_home_handler))
-        .route("/home", get(handler::home_page_handler))
-        .route("/post/create", post(handler::create_post_handler))
+        .route("/api/posts/create", post(handler::create_post_handler))
         .with_state(app_state)
 }
