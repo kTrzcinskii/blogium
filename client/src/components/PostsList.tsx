7196a7cdf4085d81912cf2aa773c61c71aaea8b0
kTrzcinskii <kt.trzcinski@gmail.com>
2024-10-29 22:49:52 +0100

    Call `fetchNextPage` when scrolled to the bottom of the posts list

--- client/src/components/PostsList.tsx
+++ client/src/components/PostsList.tsx
@@ -4,10 +4,47 @@ import PostCardSkeleton from './PostCardSkeleton';
 import { POST_FETCH_COUNT_LIMIT } from '@/const';
 import { AlertCircle } from 'lucide-react';
 import { Alert, AlertTitle, AlertDescription } from './ui/alert';
+import { useCallback, useRef } from 'react';
 
 const PostsList = () => {
-    const { data, fetchNextPage, hasNextPage, status, isFetchingNextPage } =
-        useFetchPosts();
+    const {
+        data,
+        fetchNextPage,
+        hasNextPage,
+        status,
+        isFetchingNextPage,
+        isLoading,
+    } = useFetchPosts();
+
+    // https://dev.to/kevin-uehara/2-react-query-infinite-scroll-1mg8
+    const observer = useRef<IntersectionObserver>();
+    const lastElementRef = useCallback(
+        (node: HTMLDivElement) => {
+            if (isLoading) {
+                return;
+            }
+
+            if (observer.current) {
+                observer.current.disconnect();
+            }
+
+            observer.current = new IntersectionObserver((entries) => {
+                if (
+                    entries[0].isIntersecting &&
+                    hasNextPage &&
+                    !isFetchingNextPage
+                ) {
+                    console.log('fetching new page');
+                    fetchNextPage();
+                }
+            });
+
+            if (node) {
+                observer.current.observe(node);
+            }
+        },
+        [fetchNextPage, hasNextPage, isFetchingNextPage, isLoading],
+    );
 
     if (status === 'pending') {
         return (
@@ -40,17 +77,19 @@ const PostsList = () => {
             {data.pages.map((page) =>
                 page.data.map((post) => {
                     return (
-                        <PostCard
-                            key={`${post.posted_at}-${post.username}`}
-                            {...post}
-                        />
+                        <div ref={lastElementRef}>
+                            <PostCard
+                                key={`${post.posted_at}-${post.username}`}
+                                {...post}
+                            />
+                        </div>
                     );
                 }),
             )}
-            {/* TODO: it should be some element that when scrolled to it calls fetchNextPage */}
-            {hasNextPage && !isFetchingNextPage && (
-                <button onClick={() => fetchNextPage()}>Fetch more</button>
-            )}
+            {isFetchingNextPage &&
+                Array.from({ length: 3 }, (_, index) => (
+                    <PostCardSkeleton key={index} />
+                ))}
         </div>
     );
 };
