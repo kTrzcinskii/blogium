c42325752c437b28b15978b27d01f6b156d50f54
kTrzcinskii <kt.trzcinski@gmail.com>
2024-10-29 21:12:13 +0100

    Add error message for failed posts fetching

--- client/src/components/PostsList.tsx
+++ client/src/components/PostsList.tsx
@@ -2,6 +2,8 @@ import { useFetchPosts } from '@/api/queries/useFetchPosts';
 import PostCard from './PostCard';
 import PostCardSkeleton from './PostCardSkeleton';
 import { POST_FETCH_COUNT_LIMIT } from '@/const';
+import { AlertCircle } from 'lucide-react';
+import { Alert, AlertTitle, AlertDescription } from './ui/alert';
 
 const PostsList = () => {
     const { data, fetchNextPage, hasNextPage, status, isFetchingNextPage } =
@@ -21,7 +23,16 @@ const PostsList = () => {
     }
 
     if (status === 'error') {
-        return <div>TODO: add some error message</div>;
+        return (
+            <Alert variant="destructive" className="my-7">
+                <AlertCircle className="h-4 w-4" />
+                <AlertTitle>Error</AlertTitle>
+                <AlertDescription>
+                    Something went wrong while fetching posts. Please try again
+                    later.
+                </AlertDescription>
+            </Alert>
+        );
     }
 
     return (
